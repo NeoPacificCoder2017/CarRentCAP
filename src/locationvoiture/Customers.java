838f77aa875ef0c363b3bacf83ad17ce18024ce1
Parea <taie.taataparea1@gmail.com>
2018-04-20 10:27:31 -1000

    Modified

--- src/locationvoiture/Customers.java
+++ src/locationvoiture/Customers.java
@@ -3,47 +3,10 @@ package locationvoiture;
 
 public class Customers {
     // Propriétés
-    protected String nom, prenom;
-    protected int age, noPermis;
+
     
     // Constructeur avec arguments
-     public Customers(String nom, String prenom, int age, int noPermis) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-        this.noPermis = noPermis;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getNoPermis() {
-        return noPermis;
-    }
-
-    public void setNoPermis(int noPermis) {
-        this.noPermis = noPermis;
+     public Customers() {
     }
     
     
