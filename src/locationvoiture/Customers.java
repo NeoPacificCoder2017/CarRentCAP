6f0ce583d5e22e131fec2c5dce195a0093c32da4
Parea <taie.taataparea1@gmail.com>
2018-04-20 10:39:57 -1000

    Modified

--- src/locationvoiture/Customers.java
+++ src/locationvoiture/Customers.java
@@ -3,10 +3,14 @@ package locationvoiture;
 
 public class Customers {
     // Propriétés
-
+    protected String nom;
+    protected String prenom;
+    protected int age;
+    protected int nPermis;
     
     // Constructeur avec arguments
      public Customers() {
+       
     }
     
     
