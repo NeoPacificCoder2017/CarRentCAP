c9eeb4b9bf4732bcf93e59035895b9165708ae3e
skat987 <ctuhiti@gmail.com>
2018-04-30 12:56:35 -1000

    modif main + déclaration de Scanner en global

--- src/locationvoiture/LocationVoiture.java
+++ src/locationvoiture/LocationVoiture.java
@@ -7,31 +7,12 @@ import java.util.*;
  */
 public class LocationVoiture {
   
+    private static Scanner scan = new Scanner(System.in);
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-        Scanner saisie = new Scanner(System.in);
-        int noImma, nbPassagerMax, prixJour, km, nbLocation;
-        boolean statut;
-        
-        // Menu 
-        
-        // Demande de saisie de l'utilisateur pour un nouveau véhicule
-        print(" Entrez le numéro immatricualation :");
-        noImma = saisie.nextInt();
-        print(" Entrez le nombre de passager maximum :");
-        nbPassagerMax = saisie.nextInt();
-        print(" Entrez le numéro immatricualation :");
-        noImma = saisie.nextInt();
-        print(" Entrez le numéro immatricualation :");
-        noImma = saisie.nextInt();
-        print(" Entrez le numéro immatricualation :");
-        noImma = saisie.nextInt();
-        print(" Entrez le numéro immatricualation :");
-        noImma = saisie.nextInt();
-        
+    public static void main(String[] args) {      
         
     }
     
