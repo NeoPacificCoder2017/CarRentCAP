8fc1fcdd117902f985a02b5206986bce48ea1881
Parea <taie.taataparea1@gmail.com>
2018-04-30 10:43:34 -1000

    init: branch Customers

--- src/locationvoiture/Customers.java
+++ src/locationvoiture/Customers.java
@@ -3,16 +3,52 @@ package locationvoiture;
 
 public class Customers {
     // Propriétés
-    protected String nom;
-    protected String prenom;
+    protected String lastname;
+    protected String firstname;
     protected int age;
-    protected int nPermis;
+    protected int numberLicence;
     
     // Constructeur avec arguments
-     public Customers() {
-       
+     public Customers(String lastname, String firstname, int age, int numberLicence) {
+       this.lastname = lastname;
+       this.firstname = firstname;
+       this.age = age;
+       this.numberLicence = numberLicence;
     }
-    
-    
+     
+     //Accesseurs 
+     public String getLastname(){
+       return this.lastname;
+     }
+     
+     public void setLastname(String lastname ){
+       this.lastname = lastname;
+     }
+     
+     public String getFirstname(){
+       return this.firstname;
+     }
+     
+     public void setFirstname(String firstname ){
+       this.firstname = firstname;
+     }
+     
+     public int getAge(){
+       return this.age;
+     }
+     
+     public void setage(int age ){
+       this.age = age;
+     }
+     
+     public int getNumLicence(){
+       return this.numberLicence;
+     }
+     
+     public void setNumberLicence(int numberLicence ){
+       this.numberLicence = numberLicence;
+     }
+     
+     //Fonction qui seras appeler dans le "Main"
     
 }
--- src/locationvoiture/LocationVoiture.java
+++ src/locationvoiture/LocationVoiture.java
@@ -1,22 +1,100 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package locationvoiture;
+import java.util.*;
 
-/**
- *
- * @author eleve9
- */
+
 public class LocationVoiture {
-
-    /**
-     * @param args the command line arguments
-     */
+  
+    private static final Scanner scan = new Scanner(System.in);
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+    ArrayList<Customers> customer = new ArrayList<Customers>();
+       
+      int option = -1;
+
+      while(option != 0) {
+        Menu();
+        option = scan.nextInt();
         
+        switch(option) {
+          case 1:
+            
+            break;
+          case 2:
+            Customers c = addNewCustomer();
+              customer.add(c);
+              break;
+          case 3:
+            listCustomer(customer);
+            option = scan.nextInt();
+            break;
+          case 4:
+              
+            break;
+          default:
+            println("*********************************");
+              if( option == 0){
+                println("Merci");
+              }else{
+                println ("Choix incorrect !");
+              }
+        }
+      }
+      
     }
     
+    //////////////////////////////Function///////////////////////////////////////
+    
+    public static void println(Object a){
+        System.out.println(a);
+    }
+    
+    public static void print(Object a){
+        System.out.print(a);
+    }
+    
+    public static void Menu(){
+      println("*********************************");
+      println("Veuillez sélectionner une option:"
+              + "\n 1-Acquerir un véhicule"
+              + "\n 2-Enregistrer nouveau client"
+              + "\n 3-Louer véhicule"
+              + "\n 4-Rendre véhicule"
+              + "\n 0-Quitter");
+      print("Votre choix: ");
+    }
+    
+    //////////////////////////////Ajouter un nouveau client///////////////////////////////////////
+    
+    public static Customers addNewCustomer(){
+      println("*********************************"
+        + "\n*********************************");
+      println("Ajouter un nouveau client: ");
+        
+      print("Nom: ");
+        String lastName = scan.next();
+        
+      print("Prénom: ");
+        String firstName = scan.next();
+      
+      print("Age: ");
+        int age = scan.nextInt();
+      
+      print("N° Permis: ");
+        int numberLicence = scan.nextInt();
+        return new Customers(firstName, lastName, age, numberLicence);
+      
+    }
+    
+    ////////////////////////////Affichage des client enregistrer/////////////////////////////////////
+    
+    public static void listCustomer(ArrayList<Customers> customers){
+      int index = 0;
+      String space = " ";
+      println("Voici la liste des clients1: ");
+        for(Customers customer : customers ){
+          index++;
+            println("\n" + index + space + customer.getLastname() + space + customer.getFirstname() + space + customer.getAge() + " ans numéro de permis " + customer.getNumLicence() );
+        }
+    }
 }
