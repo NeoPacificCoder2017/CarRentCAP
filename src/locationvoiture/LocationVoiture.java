b9336495c7e062c430fd080e9c49dc067326a0de
skat987 <ctuhiti@gmail.com>
2018-05-02 10:18:28 -1000

    update afficheVehicule

--- src/locationvoiture/LocationVoiture.java
+++ src/locationvoiture/LocationVoiture.java
@@ -6,20 +6,13 @@ import java.util.*;
  * @author eleve9
  */
 public class LocationVoiture {
-<<<<<<< HEAD
   
     private static Scanner scan = new Scanner(System.in);
     
     /**
      * @param args the command line arguments
-     */
-    public static void main(String[] args) {      
-=======
-
-    private static Scanner scan = new Scanner(System.in);
-    
-    public static void main(String[] args) {
-        
+     */    
+    public static void main(String[] args) {        
         ArrayList<Rent> stockCars = new ArrayList<Rent>();
     }
     
@@ -39,7 +32,6 @@ public class LocationVoiture {
                 print("\nLe numéro saisie n'existe pas, veuillez réessayer.");
             }
         }while(rent == null);
->>>>>>> 3493f134bb3fd29a98580a1a370506400fed5281
         
         print("\nLe montant à payer est de : " + rent.getCost());
     }
